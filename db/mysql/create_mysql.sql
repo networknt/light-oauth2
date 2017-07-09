0ffe9dcb1b7fe4beacadedc817703503354f8a5a
Steve Hu <stevehu@gmail.com>
2017-07-08 21:55:11 -0400

    update mysql script to add client table for OpenID connect

--- db/mysql/create_mysql.sql
+++ db/mysql/create_mysql.sql
@@ -52,6 +52,62 @@ CREATE TABLE services (
 )
 ENGINE=INNODB;
 
+CREATE TABLE IF NOT EXISTS client (
+  client_id VARCHAR(32) NOT NULL,
+  client_secret VARCHAR(512) NOT NULL,
+  client_type VARCHAR(12) NOT NULL, -- public confidential trusted
+  client_description VARCHAR(1024),
+  reuse_refresh_tokens BOOLEAN DEFAULT true NOT NULL,
+  dynamically_registered BOOLEAN DEFAULT false NOT NULL,
+  allow_introspection BOOLEAN DEFAULT false NOT NULL,
+  id_token_validity_seconds BIGINT DEFAULT 600 NOT NULL,
+  access_token_validity_seconds BIGINT,
+  refresh_token_validity_seconds BIGINT,
+
+  response_types VARCHAR(256),  -- response type values that the client is declaring that it will use. separated by space
+  grant_type VARCHAR(256), -- grant types that the client is declaring it will use. separated by space
+  application_type VARCHAR(256), -- kind of the application native or web. If omitted, web is used as the default.
+  client_name VARCHAR(255), -- name of the client to be presented to the end-user on the log in page.
+  logo_uri VARCHAR(256), -- url that references a logo for the client application
+  policy_uri VARCHAR(256),  -- url that the relying party client provides to the end-user to read about how the profile data will be used.
+  client_uri VARCHAR(256),  -- url of the home page of the client
+  tos_uri VARCHAR(256),  -- url that the relying party client provides to the end-user to read about the client's terms of service
+  jwks_uri VARCHAR(256),  -- url for the client's json web key set document
+  jwks TEXT, -- client's json web key set document, passed by value
+  sector_identifier_uri VARCHAR(256),  -- url using the https schema to be used in calculating pseudonymous identifiers by the OP
+  subject_type VARCHAR(256),  -- subject type requested for responses to this client
+
+  id_token_signed_response_alg VARCHAR(256), -- jws alg algorithm required for signing the id token issued to this client
+  id_token_encrypted_response_alg VARCHAR(256),  -- jwe algorithm required for encrypting the id token issued to this client
+  id_token_encrypted_response_enc VARCHAR(256),  -- jwe enc algorithm required for encrypting the id token issued to this client
+
+  user_info_signed_response_alg VARCHAR(256), -- jws alg algorithm required for signing userinfo response
+  user_info_encrypted_response_alg VARCHAR(256),  -- jwe alg algorithm required for encrypting userinfo response
+  user_info_encrypted_response_enc VARCHAR(256), -- jwe enc algorithm required for encrypting userinfo response
+
+  request_object_signing_alg VARCHAR(256), -- jws alg algorithm that must be used for signing request objects sent to the OP
+  request_object_encryption_alg VARCHAR(256), -- jwe alg algorithm that RP is declaring that it may use for encrypting request object sent to the OP
+  request_object_encryption_enc VARCHAR(256), -- jwe enc algorithm the RP is declaring that it may use for encrypting request object sent to the OP
+
+  token_endpoint_auth_method VARCHAR(256), -- requested client authentication method for the token endpoint
+  token_endpoint_auth_signing_alg VARCHAR(256),  -- WS alg algorithm that must be used for signing the JWT used to authenticate the Client at the Token Endpoint for the private_key_jwt and client_secret_jwt authentication methods
+
+  default_max_age BIGINT, -- Default Maximum Authentication Age
+  require_auth_time BOOLEAN, -- Boolean value specifying whether the auth_time Claim in the ID Token is required.
+  default_acr_values VARCHAR(256), -- Default requested Authentication Context Class Reference values.
+  initiate_login_uri VARCHAR(256), -- URI using the https scheme that a third party can use to initiate a login by the RP.
+
+  redirect_uris VARCHAR(1024),  -- redirect URI values used by the client, may be multiple values separated by space.
+
+  created_dt DATE NOT NULL,
+  owner_id VARCHAR(32) NOT NULL, -- link to user entity by id for contact.
+
+  PRIMARY KEY (client_id)
+)
+ENGINE=INNODB;
+
+
+
 INSERT INTO users (user_id, user_type, first_name, last_name, email, password, create_dt)
 VALUES('admin', 'admin', 'admin', 'admin', 'admin@cibc.com', '1000:5b39342c202d37372c203132302c202d3132302c2034372c2032332c2034352c202d34342c202d31362c2034372c202d35392c202d35362c2039302c202d352c202d38322c202d32385d:949e6fcf9c4bb8a3d6a8c141a3a9182a572fb95fe8ccdc93b54ba53df8ef2e930f7b0348590df0d53f242ccceeae03aef6d273a34638b49c559ada110ec06992', NOW());
 
