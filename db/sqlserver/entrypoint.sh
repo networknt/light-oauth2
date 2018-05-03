ce6cff0e69d1185f515905ae2fe76c6eccc2ba37
chenyan71 <passWord11!!>
2018-05-03 15:10:11 -0400

    fixes #84

---
+++ db/sqlserver/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+database=oauth2
+wait_time=15s
+password=StrongPassw0rd
+
+# wait for SQL Server to come up
+echo importing data will start in $wait_time...
+sleep $wait_time
+echo importing data...
+
+# run the init script to create the DB and the tables in /table
+/opt/mssql-tools/bin/sqlcmd -S 0.0.0.0 -U sa -P $password -i ./create_sqlserver.sql
+
