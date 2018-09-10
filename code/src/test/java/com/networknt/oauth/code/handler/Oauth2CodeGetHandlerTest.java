ad810724bc28a075c4d879baf8f593a69650b92b
Gavin Chen <gavinyan.han@gmail.com>
2018-09-09 22:24:07 -0400

    openapi-3.0 spec enhancement

--- code/src/test/java/com/networknt/oauth/code/handler/Oauth2CodeGetHandlerTest.java
+++ code/src/test/java/com/networknt/oauth/code/handler/Oauth2CodeGetHandlerTest.java
@@ -182,6 +182,7 @@ public class Oauth2CodeGetHandlerTest {
             latch.await();
             int statusCode = reference.get().getResponseCode();
             String body = reference.get().getAttachment(Http2Client.RESPONSE_BODY);
+
             Assert.assertEquals(400, statusCode);
             if(statusCode == 400) {
                 Status status = Config.getInstance().getMapper().readValue(body, Status.class);
