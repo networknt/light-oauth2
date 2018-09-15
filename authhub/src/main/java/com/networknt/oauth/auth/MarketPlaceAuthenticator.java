782f722e673bb5b581c609e96408e30af1d99a9b
Steve Hu <stevehu@gmail.com>
2018-09-14 21:20:23 -0400

    fixes #151 sync mysql db config to light-docker oauth2

--- authhub/src/main/java/com/networknt/oauth/auth/MarketPlaceAuthenticator.java
+++ authhub/src/main/java/com/networknt/oauth/auth/MarketPlaceAuthenticator.java
@@ -15,7 +15,7 @@ import java.util.Set;
  * This is the market place authentication class. It should be defined in the authenticate_class
  * column in client table when register the market place application so that this class will be
  * invoked when a user is login from market place.
- *
+ * <p>
  * The current implementation for market place will use LDAP to do authentication and market place
  * roles configuration file for authorization. In the future, the authentication should be moved to
  * the SPNEGO/Kerberos and authorization will be from database.
@@ -33,57 +33,65 @@ public class MarketPlaceAuthenticator extends AuthenticatorBase<MarketPlaceAuth>
         LightPasswordCredential passwordCredential = (LightPasswordCredential) credential;
         char[] password = passwordCredential.getPassword();
         String userType = passwordCredential.getUserType();
-        if(USER_TYPE_EMPLOYEE.equals(userType)) {
-        	if(logger.isDebugEnabled()) logger.debug("Marketplace authenticating employee credentials");
+        if (USER_TYPE_EMPLOYEE.equals(userType)) {
+            if (logger.isDebugEnabled()) logger.debug("Marketplace authenticating employee credentials");
             // LDAP authentication and database authorization
             boolean authenticated = LdapUtil.authenticate(id, new String(password));
-            if(authenticated) {
+            if (authenticated) {
                 // get role from db and construct an account object to return.
-            	Account account = null;
-            	try{ 
-	                account = new Account() {
-	                	private Set<String> roles = GithubUtil.authorize(id);
-	                    private final Principal principal = () -> id;
-	                    @Override
-	                    public Principal getPrincipal() {
-	                        return principal;
-	                    }
-	                    @Override
-	                    public Set<String> getRoles() { return roles; }
-	                };
-              } catch (Exception e) {
-                logger.error("Exception: ", e);
-                return null;
-              }
+                Account account = null;
+                try {
+                    account = new Account() {
+                        private Set<String> roles = GithubUtil.authorize(id);
+                        private final Principal principal = () -> id;
+
+                        @Override
+                        public Principal getPrincipal() {
+                            return principal;
+                        }
+
+                        @Override
+                        public Set<String> getRoles() {
+                            return roles;
+                        }
+                    };
+                } catch (Exception e) {
+                    logger.error("Exception: ", e);
+                    return null;
+                }
                 return account;
             } else {
                 logger.error("Failed to authenticate user '" + id + "' with LDAP");
                 return null;
             }
         } else {
-        	if(logger.isDebugEnabled()) logger.debug("Marketplace authenticating public user");
+            if (logger.isDebugEnabled()) logger.debug("Marketplace authenticating public user");
             // Database authentication and authorization
-        	if ((id.equals("test")) && (new String(password).equals("123456"))) {
-        		// get role from db and construct an account object to return.
-            	Account account = null;
-            	try{ 
-            		account = new Account() {
-            			private Set<String> roles = GithubUtil.authorize(id);
+            if ((id.equals("test")) && (new String(password).equals("123456"))) {
+                // get role from db and construct an account object to return.
+                Account account = null;
+                try {
+                    account = new Account() {
+                        private Set<String> roles = GithubUtil.authorize(id);
                         private final Principal principal = () -> id;
+
                         @Override
                         public Principal getPrincipal() {
                             return principal;
                         }
+
                         @Override
-                        public Set<String> getRoles() { return roles; }
+                        public Set<String> getRoles() {
+                            return roles;
+                        }
                     };
-	              } catch (Exception e) {
-	                logger.error("Exception: ", e);
-	                return null;
-	              }
-	              return account;
-        	}
+                } catch (Exception e) {
+                    logger.error("Exception: ", e);
+                    return null;
+                }
+                return account;
+            }
         }
-		return null;
+        return null;
     }
 }
