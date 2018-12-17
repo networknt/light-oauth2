1fbb247a6985840fcc04ab330eb168417c65eeea
Daniel Zhao <xulinzh@yahoo.ca>
2018-12-17 15:39:26 -0500

    initial commit for #155

---
+++ oauth2-console/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './js/header.js';
+
+class OAuth2Console extends React.Component {
+  render() {
+    return (
+      <Header />
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <OAuth2Console />,
+  document.getElementById('root')
+);
+
---
+++ oauth2-console/src/js/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/main.css';
+
+function Header() {
+    return (
+	<header role="banner">
+          <div className="logo-text">LIGHT</div>	
+	</header>
+    );
+}
+
+export default Header;
