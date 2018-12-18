3da5ef87351b4d5076d56cdaaf035fbde0ced050
Daniel Zhao <xulinzh@yahoo.ca>
2018-12-18 16:08:33 -0500

    add changes for #155

--- oauth2-console/src/js/header/header.js
+++ oauth2-console/src/js/header/header.js
@@ -2,25 +2,66 @@ import React from 'react';
 import '../../css/main.css';
 import './header.css';
 
+const views = ["Services", "Clients", "Users"];
+
+function Logo(){
+    return (
+          <a className="logo-text" href="/">LIGHT OAuth 2</a>   
+    );
+
+}
+
+function MenuItem(props){
+    return (
+        <li>
+            <button onClick={props.onClick}>
+                {props.text}
+            </button>   
+        </li>
+    );
+}
+
+class Menu extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            activeViewId: 0
+        };
+    }
+
+    selectMenuItem(i){
+        this.setState({
+            activeViewId: i
+        });
+    }
+
+    renderMenuItem(i){
+        return (
+            <MenuItem text={views[i]} onClick={()=>this.selectMenuItem(i)} />
+        );
+    }
+
+    render(){
+        return (
+            <div>
+                <nav>
+                    <ul>
+                        {
+                            views.map((text, index)=>this.renderMenuItem(index))
+                        }
+                    </ul>
+                </nav>
+            </div>
+        );
+    }
+}
+
 function Header() {
     return (
-	<div>
-	<header role="banner">
-          <a className="logo-text" href="#">LIGHT OAuth 2</a>	
-		  <div>
-			<nav>
-				<ul>
-					<li><a href="#">Services</a></li>
-					<li><a href="#">Client</a></li>
-					<li><a href="#">Users</a></li>
-				</ul>
-			</nav>
-		  </div>
-	</header>
-	<div>
-		{process.env.REACT_APP_API_URL}
-	</div>
-	</div>
+        <header role="banner">
+            <Logo />   
+            <Menu />
+        </header>
     );
 }
 
