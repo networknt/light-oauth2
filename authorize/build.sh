ca588304c4b1bf2e24045f8e593f682a2f344e7b
Steve Hu <stevehu@gmail.com>
2019-07-10 16:25:51 -0400

    fix a typo in the build.sh for authorize

--- authorize/build.sh
+++ authorize/build.sh
@@ -43,7 +43,7 @@ cleanup() {
 publish() {
     echo "Building Docker image with version $VERSION"
     docker build -t $IMAGE_NAME:$VERSION -t $IMAGE_NAME:latest -f ./docker/Dockerfile . --no-cache=true
-    docker build -t $IMAGE_NAME:$VERSION-redhat -f ./docker/Dockerfile-Redhat . --no-cache=true
+    docker build -t $IMAGE_NAME:$VERSION-slim -f ./docker/Dockerfile-Slim . --no-cache=true
     echo "Images built with version $VERSION"
     echo "Pushing image to DockerHub"
     docker push $IMAGE_NAME
