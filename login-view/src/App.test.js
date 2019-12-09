fb5b9d9659290d29ff77c3e34b67ac05bcd54938
Steve Hu <stevehu@gmail.com>
2019-12-09 16:07:53 -0500

    fixes #217 upgrade login-view to the latest dependencies

--- login-view/src/App.test.js
+++ login-view/src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test('renders sign in', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Remember me/i);
+  expect(linkElement).toBeInTheDocument();
 });
