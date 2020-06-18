96f1b39c15ab41f14840baf20ca4fe84e5b8dfca
Steve Hu <stevehu@gmail.com>
2020-06-17 22:50:36 -0400

    fixes #272 update login-view deploy-test.sh for test cloud

--- login-view/deploy-test.sh
+++ login-view/deploy-test.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 echo "Build the view in in test mode"
 yarn build
-echo "Build completed in build folder, start copying to remote test2 server"
-scp -r ./build test2:/home/steve/light-chain/light-config-test/light-router/test-direct/signin
+echo "Build completed in build folder."
+echo "start copying to remote test1 server."
+ssh test1 "rm -rf ~/light-chain/light-config-test/test1/signin/build"
+scp -r ./build test1:/home/steve/light-chain/light-config-test/test1/signin
+echo "start copying to remote test2 server."
+ssh test2 "rm -rf ~/light-chain/light-config-test/test2/signin/build"
+scp -r ./build test2:/home/steve/light-chain/light-config-test/test2/signin
 echo "Copied!"
