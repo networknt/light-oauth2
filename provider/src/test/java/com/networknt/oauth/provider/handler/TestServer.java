67f6bebbccc04be7a1af54ec877b468fad861bca
Steve Hu <stevehu@gmail.com>
2021-06-27 22:08:41 -0400

    fixes #318 update TestServer to replace Server.config with Server.getServerConfig method

--- provider/src/test/java/com/networknt/oauth/provider/handler/TestServer.java
+++ provider/src/test/java/com/networknt/oauth/provider/handler/TestServer.java
@@ -52,7 +52,7 @@ public class TestServer extends ExternalResource {
     }
 
     public ServerConfig getServerConfig() {
-        return Server.config;
+        return Server.getServerConfig();
     }
 
     @Override
