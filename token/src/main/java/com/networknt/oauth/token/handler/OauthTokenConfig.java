c30710d7ed806f75970fd83f5fea3f8a7e8ea392
Steve Hu <stevehu@gmail.com>
2021-09-14 22:39:18 -0400

    fixes #325 implement a new grant type bootstrap_token to create long-lived static token

--- token/src/main/java/com/networknt/oauth/token/handler/OauthTokenConfig.java
+++ token/src/main/java/com/networknt/oauth/token/handler/OauthTokenConfig.java
@@ -1,7 +1,12 @@
 package com.networknt.oauth.token.handler;
 
 public class OauthTokenConfig {
+    public static final String CONFIG_NAME = "oauth-token";
     boolean enableAudit;
+    String bootstrapToken;
+    String bootstrapClientId;
+    String bootstrapClientSecret;
+    String bootstrapScope;
 
     public boolean isEnableAudit() {
         return enableAudit;
@@ -9,5 +14,37 @@ public class OauthTokenConfig {
 
     public void setEnableAudit(boolean enableAudit) {
         this.enableAudit = enableAudit;
+    }
+
+    public String getBootstrapToken() {
+        return bootstrapToken;
+    }
+
+    public void setBootstrapToken(String bootstrapToken) {
+        this.bootstrapToken = bootstrapToken;
+    }
+
+    public String getBootstrapClientId() {
+        return bootstrapClientId;
+    }
+
+    public void setBootstrapClientId(String bootstrapClientId) {
+        this.bootstrapClientId = bootstrapClientId;
+    }
+
+    public String getBootstrapClientSecret() {
+        return bootstrapClientSecret;
+    }
+
+    public void setBootstrapClientSecret(String bootstrapClientSecret) {
+        this.bootstrapClientSecret = bootstrapClientSecret;
+    }
+
+    public String getBootstrapScope() {
+        return bootstrapScope;
+    }
+
+    public void setBootstrapScope(String bootstrapScope) {
+        this.bootstrapScope = bootstrapScope;
     }
 }
